b3e7741efb4841061f80521ac3aed4da07780ba8
Talgat Medetbekov <medetbekov.t@gmail.com>
2020-05-01 10:24:19 +0600

    Verify a signature for the signed contents using the public key

--- src/test/kotlin/io/horizontalsystems/hdwalletkit/ECKeyTest.kt
+++ src/test/kotlin/io/horizontalsystems/hdwalletkit/ECKeyTest.kt
@@ -3,7 +3,9 @@ package io.horizontalsystems.hdwalletkit
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import java.io.ByteArrayOutputStream
 import java.math.BigInteger
+import java.nio.charset.StandardCharsets
 
 class ECKeyTest {
 
@@ -14,6 +16,7 @@ class ECKeyTest {
 
     @Before
     fun setUp() {
+        // address(P2PKH): 1Ms9Sv1ebeBnoQCSxmwwichbQEaZ5KfXA7
         publicKey = "037d56797fbe9aa506fc263751abf23bb46c9770181a6059096808923f0a64cb15".hexStringToByteArray()
         privateKey = BigInteger("4ee8efccaa04495d5d3ab0f847952fcff43ffc0459bd87981b6be485b92f8d64", 16)
         dataToSign = "01000000019cb78e361651edc22c1c6502961440c139038ca8a8d1696392cd002219d796f2000000001976a914e4de5d630c5cacd7af96418a8f35c411c8ff3c0688acffffffff01c0e4022a010000001976a914e4de5d630c5cacd7af96418a8f35c411c8ff3c0688ac0000000001000000".hexStringToByteArray()
@@ -32,11 +35,21 @@ class ECKeyTest {
     @Test
     fun signMessage() {
         ecKey = ECKey(publicKey, privateKey, true)
-        // address(P2PKH): 1Ms9Sv1ebeBnoQCSxmwwichbQEaZ5KfXA7
+
         val resultSignature = ecKey.signMessage("abc")
         val expectedSignature = "H5P92Zr8CMBawZmIBknEKYwtiND3f7m4nloz6inU5Yg1NHd3QsZnLGAFi0sn+leQZKYi8dgakcpSsBM3VUOf9lI="
 
         Assert.assertEquals(expectedSignature, resultSignature)
+    }
+
+    @Test
+    fun verifySignature() {
+        ecKey = ECKey(publicKey, privateKey, true)
+
+        val resultSignature = ecKey.createSignature(dataToSign)
+        val verifySignature = ecKey.verifySignature(dataToSign, resultSignature)
+
+        Assert.assertTrue(verifySignature)
     }
 
     @Test(expected = IllegalStateException::class)
