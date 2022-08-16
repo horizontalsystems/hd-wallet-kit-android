5241b077e86b5a5b50ba8e6736cb11c7d6146e2e
chyngyz <chyngyz.omurov@gmail.com>
2022-08-16 16:33:43 +0600

    Abstract mnemonic word list to a MnemonicWordList class
    - normalize mnemonic words for comparison for Spanish and French

---
+++ src/main/kotlin/io/horizontalsystems/hdwalletkit/MnemonicWordList.kt
@@ -0,0 +1,46 @@
+package io.horizontalsystems.hdwalletkit
+
+import java.text.Normalizer
+
+class MnemonicWordList(
+        private val words: List<String>,
+        private val mustBeNormalized: Boolean
+) {
+    private val normalizedWords = words.map { normalize(it) }
+
+    operator fun get(index: Int): String {
+        return words[index]
+    }
+
+    fun indexOf(word: String): Int {
+        return normalizedWords.indexOf(normalize(word))
+    }
+
+    fun validWord(word: String, partial: Boolean = false): Boolean {
+        return if (partial) {
+            startsWith(word)
+        } else {
+            contains(word)
+        }
+    }
+
+    fun validWords(words: List<String>): Boolean {
+        return words.all { contains(it) }
+    }
+
+    private fun startsWith(prefix: String): Boolean {
+        return normalizedWords.any { it.startsWith(normalize(prefix)) }
+    }
+
+    private fun contains(mnemonic: String): Boolean {
+        return normalizedWords.contains(normalize(mnemonic))
+    }
+
+    private fun normalize(string: String): String {
+        return if (mustBeNormalized)
+            Normalizer.normalize(string, Normalizer.Form.NFKD).replace("[^\\p{ASCII}]".toRegex(), "")
+        else
+            string
+    }
+
+}
