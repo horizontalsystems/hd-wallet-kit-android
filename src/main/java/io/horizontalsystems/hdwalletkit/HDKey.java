80362d952ba29d9940f1b20c658fa2f8a16ec0f7
chyngyz <chyngyz.omurov@gmail.com>
2022-10-25 12:19:38 +0600

    Remove `gapLimit` from HDWallet

--- src/main/java/io/horizontalsystems/hdwalletkit/HDKey.java
+++ src/main/java/io/horizontalsystems/hdwalletkit/HDKey.java
@@ -126,29 +126,6 @@ public class HDKey extends ECKey {
         return ser.array();
     }
 
-    /* s must be an even-length string. */
-    public static byte[] hexStringToByteArray(String s) {
-        int len = s.length();
-        byte[] data = new byte[len / 2];
-        for (int i = 0; i < len; i += 2) {
-            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-                    + Character.digit(s.charAt(i + 1), 16));
-        }
-        return data;
-    }
-
-    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-
-    public static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
-
     private String toBase58(byte[] ser) {
         return Base58.encode(addChecksum(ser));
     }
