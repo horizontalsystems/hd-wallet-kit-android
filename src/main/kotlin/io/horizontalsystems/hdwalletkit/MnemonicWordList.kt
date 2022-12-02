122a93d423573d8b184d8acee3bdb6c8182693b9
chyngyz <chyngyz.omurov@gmail.com>
2022-12-02 16:56:21 +0600

    Revert "Fix mnemonic normalization"
    This reverts commit 5b84f41b1c90cd6a13a3128f5b188c7a70de50ef.

--- src/main/kotlin/io/horizontalsystems/hdwalletkit/MnemonicWordList.kt
+++ src/main/kotlin/io/horizontalsystems/hdwalletkit/MnemonicWordList.kt
@@ -3,15 +3,17 @@ package io.horizontalsystems.hdwalletkit
 import java.text.Normalizer
 
 class MnemonicWordList(
-    private val words: List<String>
+        private val words: List<String>,
+        private val mustBeNormalized: Boolean
 ) {
+    private val normalizedWords = words.map { normalize(it) }
 
     operator fun get(index: Int): String {
         return words[index]
     }
 
     fun indexOf(word: String): Int {
-        return words.indexOf(normalize(word))
+        return normalizedWords.indexOf(normalize(word))
     }
 
     fun validWord(word: String, partial: Boolean = false): Boolean {
@@ -27,24 +29,25 @@ class MnemonicWordList(
     }
 
     private fun startsWith(prefix: String): Boolean {
-        return words.any { it.startsWith(normalize(prefix)) }
+        return normalizedWords.any { it.startsWith(normalize(prefix)) }
     }
 
     private fun contains(mnemonic: String): Boolean {
-        return words.contains(normalize(mnemonic))
+        return normalizedWords.contains(normalize(mnemonic))
     }
 
-    companion object {
-        fun normalize(string: String): String {
-                return Normalizer.normalize(string, Normalizer.Form.NFKD)
-        }
+    private fun normalize(string: String): String {
+        return if (mustBeNormalized)
+            Normalizer.normalize(string, Normalizer.Form.NFKD).replace("[^\\p{ASCII}]".toRegex(), "")
+        else
+            string
     }
 
     fun fetchSuggestions(input: String): List<String> {
         val suggestions = mutableListOf<String>()
         val normalizedInput = normalize(input)
 
-        for (word in words) {
+        for (word in normalizedWords) {
             if (word.startsWith(normalizedInput)) {
                 suggestions.add(word)
             }
